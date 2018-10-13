01e7fadd5f59bc704c5088dde4962de38a96f2cd
Ogi wan Kenobi <scekicogi@gmail.com>
2018-10-13 18:27:37 +0200

    --Primary activity_main layout design

--- app/src/main/java/com/example/android/codetovote/MainActivity.java
+++ app/src/main/java/com/example/android/codetovote/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.android.codetovote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView add_questionIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add_questionIcon = findViewById(R.id.add_question);
+
+
+
+        add_questionIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
 
 
 
