ca9341605d34f6281d42c017e6f7d6637c8bb1c2
Ogi wan Kenobi <scekicogi@gmail.com>
2018-10-14 00:33:21 +0200

    --Problems 4 and 5 activities ans layouts created

---
+++ app/src/main/java/com/example/android/codetovote/activity/Problem4Activity.java
@@ -0,0 +1,17 @@
+package com.example.android.codetovote.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.android.codetovote.R;
+
+public class Problem4Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem4);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/android/codetovote/activity/Problem5Activity.java
@@ -0,0 +1,16 @@
+package com.example.android.codetovote.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.android.codetovote.R;
+
+public class Problem5Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem5);
+
+    }
+}
