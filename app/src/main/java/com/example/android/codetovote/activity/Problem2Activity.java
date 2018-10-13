9a649ff0a20ca6c8ce908a49534dfd31e2cfb09a
Ogi wan Kenobi <scekicogi@gmail.com>
2018-10-14 00:24:23 +0200

    --Problem 3 implemented

--- app/src/main/java/com/example/android/codetovote/activity/Problem2Activity.java
+++ app/src/main/java/com/example/android/codetovote/activity/Problem2Activity.java
@@ -1,16 +1,35 @@
 package com.example.android.codetovote.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.android.codetovote.R;
 
 public class Problem2Activity extends Activity {
 
+
+    private TextView mestoGlasanjaLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem2);
+
+        mestoGlasanjaLink = findViewById(R.id.P2TextView2);
+
+        mestoGlasanjaLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://birackispisak.mduls.gov.rs/javniportal";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/android/codetovote/activity/Problem3Activity.java
+++ app/src/main/java/com/example/android/codetovote/activity/Problem3Activity.java
@@ -1,17 +1,58 @@
 package com.example.android.codetovote.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.android.codetovote.R;
 
 public class Problem3Activity extends Activity {
+
+    TextView link1, link2, link3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem3);
 
+
+        link1 = findViewById(R.id.P3TextView2);
+        link2 = findViewById(R.id.P3TextView4);
+        link3 = findViewById(R.id.P3TextView6);
+
+
+        link1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "http://www.parlament.gov.rs/narodna-skupstina-/sastav/politicke-stranke/politicke-stranke.904.html";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        link2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.istinomer.rs/pregled_ocena/pstr,10/strana,1/";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        link3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.istinomer.rs/pregled_aktera/pstr,10/strana,1/";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
 
     }
 }
